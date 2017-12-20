85160d368bf35e5767e51810716b59035529df9c
jorge-miguel <carrara@uninove.edu.br>
2017-12-20 00:22:08 -0200

    Tela do painel administrativo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,165 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+<div class="container-fluid">
+   <h1 class="ls-title-intro ls-ico-dashboard">Meu painel</h1>
+   <div class="ls-box ls-board-box">
+      <header class="ls-info-header">
+         <p class="ls-float-right ls-float-none-xs ls-small-info">Valido até <strong>01.05.2014</strong></p>
+         <h2 class="ls-title-3">Estatísticas do avaliador</h2>
+      </header>
+      <div id="sending-stats" class="row">
+         <div class="col-sm-6 col-md-3">
+            <div class="ls-box">
+               <div class="ls-box-head">
+                  <h6 class="ls-title-4">Empresas cadastradas</h6>
+               </div>
+               <div class="ls-box-body">
+                  <span class="ls-board-data">
+                  <strong>10</strong>
+                  </span>
+               </div>
+               <div class="ls-box-footer">
+                  <a href="#" class="ls-btn ls-btn-xs">Cadastrar</a>
+               </div>
             </div>
-        </div>
-    </div>
+         </div>
+         <div class="col-sm-6 col-md-3">
+            <div class="ls-box">
+               <div class="ls-box-head">
+                  <h6 class="ls-title-4">Total de departamentos</h6>
+               </div>
+               <div class="ls-box-body">
+                  <span class="ls-board-data">
+                  <strong>0</strong>
+                  </span>
+               </div>
+               <div class="ls-box-footer">
+                  <a href="#" class="ls-btn ls-btn-xs">Cadastrar</a>
+               </div>
+            </div>
+         </div>
+         <div class="col-sm-6 col-md-3">
+            <div class="ls-box">
+               <div class="ls-box-head">
+                  <h6 class="ls-title-4">Total de colaboradores</h6>
+               </div>
+               <div class="ls-box-body">
+                  <span class="ls-board-data">
+                  <strong>10.743</strong>
+                  </span>
+               </div>
+            </div>
+         </div>
+         <div class="col-sm-6 col-md-3">
+            <div class="ls-box">
+               <div class="ls-box-head">
+                  <h6 class="ls-title-4 color-default">Disponível</h6>
+               </div>
+               <div class="ls-box-body">
+                  <span class="ls-board-data">
+                  <strong class="ls-color-theme">81%</strong>
+                  <small>989.257</small>
+                  </span>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="row">
+      <div class="col-md-6">
+         <div class="ls-box">
+            <header class="ls-info-header">
+               <h2 class="ls-title-3">Gerenciar empresas</h2>
+               <a href="#" class="ls-btn ls-btn-sm">Ver todas</a>
+            </header>
+            <table class="ls-table">
+               <thead>
+                  <th>Nome da empresa</th>
+                  <th>Data de cadastro</th>
+               </thead>
+               <tbody>
+                  <tr>
+                     <td><a href="/locawebstyle/documentacao/exemplos/painel1/client">CRECISP</a> </td>
+                     <td>19/12/2017</td>
+                  </tr>
+               </tbody>
+            </table>
+         </div>
+      </div>
+      <div class="col-md-6">
+         <div class="ls-box">
+            <header class="ls-info-header">
+               <h2 class="ls-title-3">Avaliações de desempenho</h2>
+               <a href="/locawebstyle/documentacao/exemplos/painel1/clients" class="ls-btn ls-btn-sm">Ver todos</a>
+            </header>
+            <table class="ls-table">
+               <thead>
+                  <th>Nome da avaliação</th>
+                  <th>Status</th>
+
+               </thead>
+               <tbody>
+                  <tr>
+                     <td><a href="/locawebstyle/documentacao/exemplos/painel1/client">Avaliação de desempenho 001</a> </td>
+                     <td><a href="/locawebstyle/documentacao/exemplos/painel1/client">Em andamento</a> </td>
+                  </tr>
+                  <tr>
+                     <td><a href="/locawebstyle/documentacao/exemplos/painel1/client">Avaliação de desempenho 001</a> </td>
+                     <td><a href="/locawebstyle/documentacao/exemplos/painel1/client">Em andamento</a> </td>
+                  </tr>
+               </tbody>
+            </table>
+         </div>
+      </div>
+   </div>
+</div>
+<footer class="ls-footer" role="contentinfo">
+   <nav class="ls-footer-menu">
+      <h2 class="ls-title-footer">suporte e ajuda</h2>
+      <ul class="ls-footer-list">
+         <li>
+            <a href="#" target="_blank" class="bg-customer-support">
+            <span class="visible-lg">Atendimento</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" target="_blank" class="bg-my-tickets">
+            <span class="visible-lg">Meus Chamados</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" target="_blank" class="bg-help-desk">
+            <span class="visible-lg">Central de Ajuda (Wiki)</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" target="_blank" class="bg-statusblog">
+            <span class="visible-lg">Statusblog</span>
+            </a>
+         </li>
+      </ul>
+   </nav>
+   <div class="ls-footer-info">
+      <span class="last-access ls-ico-screen"><strong>Último acesso: </strong>99/99/9999 99:99:99</span>
+      <div class="set-ip"><strong>IP:</strong> 000.00.00.000</div>
+      <p class="ls-copy-right">Copyright © 1997-2017 Serviços de Internet S/A.</p>
+   </div>
+</footer>
+<div class="container">
+   <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+         <div class="panel panel-default">
+            <div class="panel-heading">Dashboard</div>
+            <div class="panel-body">
+               @if (session('status'))
+               <div class="alert alert-success">
+                  {{ session('status') }}
+               </div>
+               @endif
+               You are logged in!
+            </div>
+         </div>
+      </div>
+   </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,141 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/stylesheets/locastyle.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+<html lang="{{ app()->getLocale() }}" class="ls-theme-blue ls-html-nobg">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+      <!-- CSRF Token -->
+      <meta name="csrf-token" content="{{ csrf_token() }}">
+      <title>{{ config('app.name', 'Laravel') }}</title>
+      <meta name="mobile-web-app-capable" content="yes">
+      <meta name="apple-mobile-web-app-capable" content="yes">
+      <link rel="icon" sizes="192x192" href="/locawebstyle/assets/images/ico-painel1.png">
+      <link rel="apple-touch-icon" href="/locawebstyle/assets/images/ico-painel1.png">
+      <meta name="apple-mobile-web-app-title" content="Painel 1">
+      <script src="http://opensource.locaweb.com.br/locawebstyle/assets/javascripts/libs/mediaqueries-ie.js" type="text/javascript"></script>
+      <script src="http://opensource.locaweb.com.br/locawebstyle/assets/javascripts/libs/html5shiv.js" type="text/javascript"></script>
+      <link href="{{ asset('assets/stylesheets/locastyle.css') }}" rel="stylesheet">
+   </head>
+   <body class="documentacao documentacao_exemplos documentacao_exemplos_painel1 documentacao_exemplos_painel1_home documentacao_exemplos_painel1_home_index">
+      <div class="ls-topbar ">
+         <!-- Barra de Notificações -->
+         <div class="ls-notification-topbar">
+            <!-- Links de apoio -->
+            <div class="ls-alerts-list">
+               <a href="#" class="ls-ico-bell-o" data-counter="8" data-ls-module="topbarCurtain" data-target="#ls-notification-curtain"><span>Notificações</span></a>
+               <a href="#" class="ls-ico-bullhorn" data-ls-module="topbarCurtain" data-target="#ls-help-curtain"><span>Ajuda</span></a>
+               <a href="#" class="ls-ico-question" data-ls-module="topbarCurtain" data-target="#ls-feedback-curtain"><span>Sugestões</span></a>
             </div>
-        </nav>
-        @yield('content')
-    </div>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ asset('assets/javascripts/locastyle.js') }}"></script>
-</body>
+            <!-- Dropdown com detalhes da conta de usuário -->
+            <div data-ls-module="dropdown" class="ls-dropdown ls-user-account">
+               <a href="#" class="ls-ico-user">
+               <img src="/locawebstyle/assets/images/locastyle/avatar-example.jpg" alt="" />
+               <span class="ls-name">{{ Auth::user()->name }}</span>
+               </a>
+               <nav class="ls-dropdown-nav ls-user-menu">
+                  <ul>
+                     <li><a href="{{ url('perfil') }}">Meus dados</a></li>
+                     <li>
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sair</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+                     </li>
+                  </ul>
+               </nav>
+            </div>
+         </div>
+         <span class="ls-show-sidebar ls-ico-menu"></span>
+         <a href="/locawebstyle/documentacao/exemplos/painel1/pre-painel"  class="ls-go-next"><span class="ls-text">Voltar à lista de serviços</span></a>
+         <!-- Nome do produto/marca com sidebar -->
+         <h1 class="ls-brand-name">
+            <a href="home" class="ls-ico-chart-bar-up">
+            <small>Skill performance evaluation</small>
+            Avaliação de desempenho
+            </a>
+         </h1>
+         <!-- Nome do produto/marca sem sidebar quando for o pre-painel  -->
+      </div>
+      <aside class="ls-sidebar">
+         <div class="ls-sidebar-inner">
+            <a href="/locawebstyle/documentacao/exemplos/painel1/pre-painel"  class="ls-go-prev"><span class="ls-text">Voltar à lista de serviços</span></a>
+            <nav class="ls-menu">
+               <ul>
+                  <li><a href="/locawebstyle/documentacao/exemplos/painel1/home" class="ls-ico-dashboard" title="Meu painel">Meu painel</a></li>
+                  <li><a href="/locawebstyle/documentacao/exemplos/painel1/clients" class="ls-ico-users" title="Clientes">Clientes</a></li>
+                  <li><a href="/locawebstyle/documentacao/exemplos/painel1/stats" class="ls-ico-stats" title="Relatórios da revenda">Relatórios da revenda</a></li>
+                  <li>
+                     <a href="#" class="ls-ico-cog" title="Configurações">Configurações</a>
+                     <ul>
+                        <li><a href="/locawebstyle/documentacao/exemplos/painel1/config-domain">Domínios da Revenda</a></li>
+                        <li><a href="/locawebstyle/documentacao/exemplos/painel1/config-email">E-mail de Remetente</a></li>
+                        <li><a href="/locawebstyle/documentacao/exemplos/painel1/config-aspect">Aparência</a></li>
+                        <li><a href="/locawebstyle/documentacao/exemplos/painel1/config-answer">Atendimento</a></li>
+                        <li><a href="/locawebstyle/documentacao/exemplos/painel1/config-api">Chave de acesso para API</a></li>
+                     </ul>
+                  </li>
+               </ul>
+            </nav>
+         </div>
+      </aside>
+      <main class="ls-main ">
+         @yield('content')
+         @if(Auth::user()->tipo == 'Administrador')
+         //LIBERE ALGUMA PAGINA
+         @endif
+      </main>
+      <aside class="ls-notification">
+         <nav class="ls-notification-list" id="ls-notification-curtain">
+            <h3 class="ls-title-2">Notificações</h3>
+            <ul>
+               <li class="ls-dismissable">
+                  <a href="#">Aut ex quia et et aut necessitatibus hic dolores consequatur quis</a>
+                  <a href="#" data-ls-module="dismiss" class="ls-ico-close ls-close-notification"></a>
+               </li>
+               <li class="ls-dismissable">
+                  <a href="#">Quis et sapiente quis in nihil alias repellat id et voluptatem quis unde omnis</a>
+                  <a href="#" data-ls-module="dismiss" class="ls-ico-close ls-close-notification"></a>
+               </li>
+               <li class="ls-dismissable">
+                  <a href="#">Eius labore numquam facere et voluptates quis quo dolorem</a>
+                  <a href="#" data-ls-module="dismiss" class="ls-ico-close ls-close-notification"></a>
+               </li>
+               <li class="ls-dismissable">
+                  <a href="#">In maiores deleniti officia sed et et totam perspiciatis iste ea ex nihil</a>
+                  <a href="#" data-ls-module="dismiss" class="ls-ico-close ls-close-notification"></a>
+               </li>
+               <li class="ls-dismissable">
+                  <a href="#">Odit repellendus ut quibusdam similique impedit</a>
+                  <a href="#" data-ls-module="dismiss" class="ls-ico-close ls-close-notification"></a>
+               </li>
+            </ul>
+            <p class="ls-no-notification-message">Não há notificações.</p>
+         </nav>
+         <nav class="ls-notification-list" id="ls-help-curtain">
+            <h3 class="ls-title-2">Feedback</h3>
+            <ul>
+               <li><a href="#">> sit sequi explicabo totam unde eos</a></li>
+               <li><a href="#">> facere modi sed fugit fugiat explicabo</a></li>
+            </ul>
+         </nav>
+         <nav class="ls-notification-list" id="ls-feedback-curtain">
+            <h3 class="ls-title-2">Ajuda</h3>
+            <ul>
+               <li class="ls-txt-center hidden-xs">
+                  <a href="#" class="ls-btn-dark ls-btn-tour">Fazer um Tour</a>
+               </li>
+               <li><a href="#">> Guia</a></li>
+               <li><a href="#">> Wiki</a></li>
+            </ul>
+         </nav>
+      </aside>
+      <script src="http://opensource.locaweb.com.br/locawebstyle/assets/javascripts/libs/jquery-2.1.0.min.js" type="text/javascript"></script>
+      <script src="http://opensource.locaweb.com.br/locawebstyle/assets/javascripts/example.js" type="text/javascript"></script>
+      <script src="{{ asset('assets/javascripts/locastyle.js') }}"></script>
+      <script src="//code.highcharts.com/highcharts.js" type="text/javascript"></script>
+      <script src="http://opensource.locaweb.com.br/locawebstyle/assets/javascripts/docs/panel-charts.js" type="text/javascript"></script>
+      <script type="text/javascript">
+         $(window).on('load', function() {
+           locastyle.browserUnsupportedBar.init();
+         });
+      </script>
+   </body>
 </html>
